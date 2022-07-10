8eecd037b3b87d97cf968351d0580750f7137355
Andrew Klenner <atklenner@gmail.com>
2022-07-09 17:11:13 -0700

    most difficult way to dim an LED

--- potentiometer.py
+++ potentiometer.py
@@ -2,9 +2,10 @@ import machine
 import time
 
 pot = machine.ADC(28)
+led = machine.PWM(machine.Pin(15))
+led.freq(1000)
 conversion_factor = 3.3 / 65535
 
 while True:
-    voltage = pot.read_u16() * conversion_factor
-    print(voltage)
-    time.sleep(0.5)
+    led.duty_u16(pot.read_u16())
+    
