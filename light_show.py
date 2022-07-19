caa494230409fb1914112ec96b1746cb5d6ed578
Andrew Klenner <atklenner@gmail.com>
2022-07-18 20:04:29 -0700

    refactor

--- light_show.py
+++ light_show.py
@@ -1,13 +1,19 @@
+from lib2to3.pgen2.token import NUMBER
 from machine import Pin, ADC
 from random import randint
-import time
+from time import sleep
 
+# initialize all pins
+NUMBER_OF_LEDS = 10
 leds = []
-for i in range(10):
+for i in range(NUMBER_OF_LEDS):
     leds.append(Pin(i, Pin.OUT))
 
 dial = ADC(28)
 half_time = Pin(14, Pin.IN, Pin.PULL_DOWN)
+reset_button = Pin(13, Pin.IN, Pin.PULL_DOWN)
+current_pin = 0
+reverse = False
 
 # converts ADC output to how long a beat is in seconds
 def bpm_conversion(value):
@@ -20,18 +26,26 @@ def bpm_conversion(value):
 # 4ppqn
 def delay_time():
     # running this line four times to improve responsiveness
-    time.sleep(bpm_conversion(dial.read_u16()) / 4)
-    time.sleep(bpm_conversion(dial.read_u16()) / 4)
-    time.sleep(bpm_conversion(dial.read_u16()) / 4)
-    time.sleep(bpm_conversion(dial.read_u16()) / 4)
+    sleep(bpm_conversion(dial.read_u16()) / 4)
+    sleep(bpm_conversion(dial.read_u16()) / 4)
+    sleep(bpm_conversion(dial.read_u16()) / 4)
+    sleep(bpm_conversion(dial.read_u16()) / 4)
 
 # go though each LED from 0 to 9
 def forward_sequence():
-    for led in leds:
-        led.value(1)
-        delay_time()
-        led.value(0)
-
+    global current_pin
+    #for led in leds:
+        #led.value(1)
+        #delay_time()
+        #led.value(0)
+    leds[current_pin].value(1)
+    delay_time()
+    leds[current_pin].value(0)
+    if current_pin == NUMBER_OF_LEDS - 1:
+        current_pin = 0
+    else:
+        current_pin += 1
+    
 # go through each LED 0 through 9 then back to 1
 def knight_rider():
     for led in leds:
@@ -45,14 +59,16 @@ def knight_rider():
    
 # randomly select LEDs
 def random_sequence():
-    rand = randint(0, 9)
+    rand = randint(0, NUMBER_OF_LEDS - 1)
     leds[rand].value(1)
     delay_time()
     leds[rand].value(0)
 
 # Next I need to put 10 pins into an array and stop copy-pasting
 while True:
-    random_sequence()
+    forward_sequence()
+
+
 
 
 
